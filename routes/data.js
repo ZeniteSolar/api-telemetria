0c7842e86b4ef4405bcfb6c55e288e5f9958a469
Vinicius Cardoso <viniciuscc10@gmail.com>
2020-01-30 23:54:05 -0300

    v0.1.3 - Up to 500 log lines per time

--- routes/data.js
+++ routes/data.js
@@ -6,7 +6,7 @@ const Data = require('../models/Data');
 // Get All Data
 router.get('/', async (req, res) => {
 
-    if (req.query.limit == null ) { req.query.limit = 50 };
+    if (req.query.limit == null ) { req.query.limit = 500 };
 
     try {
         let data = await Data.find({},{ _id : 0, __v: 0 }).sort({date: -1}).limit(req.query.limit);
@@ -63,7 +63,7 @@ router.post('/insert/many', async (req, res) => {
 // Get a Specific Data
 router.get('/:mod', async (req, res) => {
 
-    if (req.query.limit == null ) { req.query.limit = 50 };
+    if (req.query.limit == null ) { req.query.limit = 500 };
 
     try {
         let data = await Data.find({ mod: req.params.mod }).sort({date: -1}).limit(req.query.limit);        
@@ -75,8 +75,7 @@ router.get('/:mod', async (req, res) => {
 
 router.get('/:mod/:top', async (req, res) => {
 
-    if (req.query.limit == null ) { req.query.limit = 50 };
-
+    if (req.query.limit == null ) { req.query.limit = 500 };
     try {
         let data = await Data.find({ mod: req.params.mod, top: req.params.top }).sort({date: -1}).limit(req.query.limit);        
         res.json(data);
